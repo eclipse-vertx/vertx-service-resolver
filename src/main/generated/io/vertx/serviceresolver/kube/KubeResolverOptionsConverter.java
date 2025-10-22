8245a8f76bf710134b7094443f8505f3331bc7c6
Julien Viet <julien@julienviet.com>
2025-10-22 12:41:11 +0000

    Set next snapshot version

--- src/main/generated/io/vertx/serviceresolver/kube/KubeResolverOptionsConverter.java
+++ src/main/generated/io/vertx/serviceresolver/kube/KubeResolverOptionsConverter.java
@@ -2,6 +2,8 @@ package io.vertx.serviceresolver.kube;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter and mapper for {@link io.vertx.serviceresolver.kube.KubeResolverOptions}.
